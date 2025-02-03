b1ab0e49473730f67b52df58a0b10f91574004b1
asifalidev77 <asifali010122@gmail.com>
2025-02-04 01:04:52 +0500

    new updates

--- src/app/_components/Hero-Section.tsx
+++ src/app/_components/Hero-Section.tsx
@@ -1,10 +1,11 @@
 export default function HeroSection() {
     return (
       <>
-      <section className="h-[85vh] w-[100%] flex justify-end items-center">
+      <section className="h-[90vh] w-[100%] flex justify-center items-center leading-[71.4px]"
+      style={{backgroundImage: "url('/bgimage.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
         <div>
-          <h1>
-            
+          <h1 className="text-[50px] md:text-[60px] lg:text-[70px] text-white text-center font-medium">
+          We Are A Digital <br /> Transformation Company
           </h1>
         </div>
       </section>
