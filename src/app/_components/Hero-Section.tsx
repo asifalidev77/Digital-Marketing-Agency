c21468d37a8b001af71934e7ba94923ed6210ea0
asifalidev77 <asifali010122@gmail.com>
2025-02-03 23:58:36 +0500

    hero-section-update

--- src/app/_components/Hero-Section.tsx
+++ src/app/_components/Hero-Section.tsx
@@ -1,6 +1,13 @@
 export default function HeroSection() {
     return (
       <>
+      <section className="h-[85vh] w-[100%] flex justify-end items-center">
+        <div>
+          <h1>
+            
+          </h1>
+        </div>
+      </section>
       </>
     );
   }
