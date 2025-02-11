4b2f4fad0a309646b2fa3d2c410299e509d5bd1a
asifalidev77 <asifali010122@gmail.com>
2025-02-11 16:40:06 -0600

    background of hiring component

--- src/app/_components/Hiring.tsx
+++ src/app/_components/Hiring.tsx
@@ -3,7 +3,7 @@ export default function Hiring() {
     return (
       <>
       <section className="h-[535px] w-[100%] flex justify-center items-center content-center"
-      style={{backgroundImage: "url('/hiring.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
+      style={{backgroundImage: "url('/missionbg.png')", backgroundSize: "cover", backgroundPosition: "center"}}>
         <div className="flex flex-col justify-center items-center gap-y-3">
           <h3 className="text-[42px] text-white text-center font-[600] leadig-[0.9em]">
           Don't let the hiring <br className="hidden md:block"/> process slow you down.
